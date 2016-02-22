b004fa2b9a7633d4a32230f8f52f797921e2cc63
asifcse10 <abuasifkhan@gmail.com>
2016-02-22 23:37:56 +0600

    Practice at home

---
+++ ActionBarTest/app/src/main/java/com/example/abuas/actionbartest/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.abuas.actionbartest;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.search_id:
+                Toast.makeText(getApplicationContext(),"Search Item Selected",Toast.LENGTH_LONG).show();
+                return  true;
+            case R.id.setting_id:
+                Toast.makeText(getApplicationContext(), "Settings Item Selected", Toast.LENGTH_LONG).show();
+                return  true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
